db36e5aa358616d68ec2845aea44ce3c39a9e4b4
karel kalang <93813597+Kharell@users.noreply.github.com>
2026-01-12 11:29:26 +0800

    lanjut video Autentifikasi part 2 di bagian Servis db

---
+++ backand_webevent/app/api/v1/users/model.js
@@ -0,0 +1,53 @@
+const mongose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Nama harus diisi"],
+            minlength: [3, "Panjang nama minimal 3 karakter"],
+            maxlength: [50, "Panjang nama maksimal 50 karakter"],
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: [true, "Email harus diisi"],
+        },
+        password: {
+            type: String,
+            required: [true, "Password harus diisi"],
+            minlength: [6, "Panjang password minimal 6 karakter"],
+        },
+        role: {
+            type: String,
+            enum: ["admin", "organizer", "owner"],
+            default: "admin",
+        },
+        organizer: {
+            type: mongose.Schema.Types.ObjectId,
+            ref: "Organizer",
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// Hash password sebelum disimpan ke database
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  next();
+});
+
+
+// Metode untuk membandingkan password saat login
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+
+module.exports = mongose.model("User", userSchema);
